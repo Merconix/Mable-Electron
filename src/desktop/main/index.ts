cf74cbbbe9c146bdaa121d640997e1705da000ca
Merc <41351832+Merconix@users.noreply.github.com>
2026-04-12 17:38:36 +0100

    Update index.ts
    Changes to enable loading a local Cinny module, based on work by NekoPavel

--- src/desktop/main/index.ts
+++ src/desktop/main/index.ts
@@ -91,30 +91,14 @@ async function createWindow(): Promise<void> {
 
   addWebContextMenu(mainWindow)
 
-  const url = getURL()
-
   await loadPlugins()
 
-  protocol.handle('https', async (req: GlobalRequest): Promise<Response> => {
-    const originalResponse = net.fetch(req, { bypassCustomProtocolHandlers: true })
-    const reqUrl = new URL(req.url)
-    // TODO: Make this check a little less specific to the way the config is set
-    if (reqUrl.host === new URL(url).host && reqUrl.pathname.endsWith('.js')) {
-      const responseVal = await originalResponse
-      let responseStr = await responseVal.text()
-      responseStr = await replaceForSource(responseStr)
-      // @ts-ignore Bugged??
-      return new Response(responseStr, {
-        headers: responseVal.headers,
-        status: responseVal.status,
-        statusText: responseVal.statusText
-      })
-    } else {
-      return originalResponse
-    }
-  })
+  // Load local file and trigger onReady when done
+  const loadPromise = app.isPackaged
+    ? mainWindow.loadFile(join(__dirname, '../../dist/index.html'))
+    : mainWindow.loadFile(join(__dirname, '../../cinny/dist/index.html'))
 
-  mainWindow.loadURL(url).then(() => {
+  loadPromise.then(() => {
     onReady()
   })
 
