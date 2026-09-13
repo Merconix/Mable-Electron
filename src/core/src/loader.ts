5d7168b564507cc350ca242ddd9f67369744f7d5
Merc <41351832+Merconix@users.noreply.github.com>
2026-09-13 18:27:26 +0100

    Update loader.ts

--- src/core/src/loader.ts
+++ src/core/src/loader.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises'
 import { join } from 'path'
-import { PatchMatch, PatchReplace, PluginExports } from '@cinny-electron/types'
+import { PatchMatch, PatchReplace, PluginExports } from '@mable-electron/types'
 
 export const pluginList: PluginExports[] = []
 
