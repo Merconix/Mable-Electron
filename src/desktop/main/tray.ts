eb216d9675f2a5a8801a88d18ae411cf94849de5
Merc <41351832+Merconix@users.noreply.github.com>
2026-09-13 18:27:40 +0100

    Update tray.ts

--- src/desktop/main/tray.ts
+++ src/desktop/main/tray.ts
@@ -3,7 +3,7 @@ import { aboutWindow, relaunch, updaterWindow } from './util'
 import normalIcon from '../../../resources/tray-icon/cinny.png?asset'
 import unreadIcon from '../../../resources/tray-icon/cinny-unread.png?asset'
 import highlightIcon from '../../../resources/tray-icon/cinny-highlight.png?asset'
-import { IpcEvents } from '@cinny-electron/core'
+import { IpcEvents } from '@mable-electron/core'
 import { allowAutoUpdates, checkForUpdates, config, quitApp, toggleWindow } from './index'
 import { quickCssPath } from './quickcss'
 
