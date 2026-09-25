20301c497bae39b8ffd162404504ad77731e0d98
Merc <41351832+Merconix@users.noreply.github.com>
2026-09-25 18:55:41 +0100

    Remove tray inject
    Removes cinny-specific tray inject

--- src/desktop/main/index.ts
+++ src/desktop/main/index.ts
@@ -11,7 +11,7 @@ import {
 import { join, resolve } from 'path'
 import { electronApp, is } from '@electron-toolkit/utils'
 import Store from 'electron-store'
-import { IpcEvents, loadPlugins, replaceForSource } from '@mable-electron/core'
+import { IpcEvents, loadPlugins } from '@mable-electron/core'
 import icon from '../../../resources/tray-icon/cinny.png?asset'
 import { createTray } from './tray'
 import { startQuickCSSWatch } from './quickcss'
@@ -93,27 +93,6 @@ async function createWindow(): Promise<void> {
 
   const url = getURL()
   
-  await loadPlugins()
-
-  protocol.handle('https', async (req: GlobalRequest): Promise<Response> => {
-    const originalResponse = net.fetch(req, { bypassCustomProtocolHandlers: true })
-    const reqUrl = new URL(req.url)
-    // TODO: Make this check a little less specific to the way the config is set
-    if (reqUrl.host === new URL(url).host && reqUrl.pathname.endsWith('.js')) {
-      const responseVal = await originalResponse
-      let responseStr = await responseVal.text()
-      responseStr = await replaceForSource(responseStr)
-      // @ts-ignore Bugged??
-      return new Response(responseStr, {
-        headers: responseVal.headers,
-        status: responseVal.status,
-        statusText: responseVal.statusText
-      })
-    } else {
-      return originalResponse
-    }
-  })
-
   mainWindow.loadURL(url).then(() => {
     onReady()
   })
